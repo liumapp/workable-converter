85b02637d027ac84380bbf578304febb5a6c9796
liumapp <liumapp.com@gmail.com>
2019-04-26 11:04:21 +0800

    test success

---
+++ src/main/java/com/liumapp/workable/converter/demo/SimpleConverter.java
@@ -0,0 +1,43 @@
+package com.liumapp.workable.converter.demo;
+
+import org.jodconverter.DocumentConverter;
+import org.jodconverter.JodConverter;
+import org.jodconverter.LocalConverter;
+import org.jodconverter.office.LocalOfficeManager;
+import org.jodconverter.office.OfficeException;
+import org.jodconverter.office.OfficeManager;
+
+import java.io.File;
+
+/**
+ * file SimpleConverter.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/4/26
+ */
+public class SimpleConverter {
+
+    public static void main (String[] args) {
+
+        File inputFile = new File("./data/test.doc");
+        File outputFile = new File("./data/test.pdf");
+
+        OfficeManager officeManager = LocalOfficeManager.builder()
+                .install()
+                .portNumbers(2002, 2003, 2004, 2005)
+                .officeHome("C:\\Program Files\\LibreOffice")
+                .build();
+
+        try {
+            officeManager.start();
+            JodConverter.convert(inputFile).to(outputFile).execute();
+        } catch (OfficeException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
