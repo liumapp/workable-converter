a59bee0ece51ad3efc3810e9e1f1d9e7163a8003
liumapp <liumapp.com@gmail.com>
2019-04-29 15:36:36 +0800

    update pom dependencies

---
+++ src/main/java/com/liumapp/workable/converter/templates/SubClassFactoryTemplate.java
@@ -0,0 +1,19 @@
+package com.liumapp.workable.converter.templates;
+
+import java.util.Set;
+
+/**
+ * file SubClassFactoryTemplate.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/4/29
+ */
+public abstract class SubClassFactoryTemplate extends FactoryTemplate {
+
+    protected static Class<?>[] toInterfaces (Class<?>[] proxiedClasses) {
+//        Set<Class<?>> interfaces = Sets
+    }
+
+}
