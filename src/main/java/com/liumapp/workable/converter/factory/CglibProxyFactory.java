12cfc7e33d40729215d89e74361ca671aa7f9dab
liumapp <liumapp.com@gmail.com>
2019-04-30 16:27:46 +0800

    add default cglib call back filter

--- src/main/java/com/liumapp/workable/converter/factory/CglibProxyFactory.java
+++ src/main/java/com/liumapp/workable/converter/factory/CglibProxyFactory.java
@@ -1,8 +1,13 @@
 package com.liumapp.workable.converter.factory;
 
+import com.liumapp.workable.converter.invoker.DefaultInvokerBridge;
 import com.liumapp.workable.converter.invoker.ObjectInvoker;
 import com.liumapp.workable.converter.templates.SubClassFactoryTemplate;
+import net.sf.cglib.proxy.CallbackFilter;
 import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.Callback;
+
+import java.lang.reflect.Method;
 
 /**
  * file CglibProxyFactory.java
@@ -14,6 +19,8 @@ import net.sf.cglib.proxy.Enhancer;
  */
 public class CglibProxyFactory extends SubClassFactoryTemplate {
 
+    private static final CallbackFilter CALLBACK_FILTERk =
+
     /**
      * 创建具体cglib的enhancer对象
      */
@@ -24,7 +31,17 @@ public class CglibProxyFactory extends SubClassFactoryTemplate {
         enhancer.setInterfaces(toInterfaces(proxiedClasses));
         enhancer.setSuperclass(getSuperClass(proxiedClasses));
         enhancer.setCallbackFilter();
+        enhancer.setCallbacks(new Callback[] {
+                new DefaultInvokerBridge(invoker)
+        });
         return null;
+    }
+
+    private static class CglibProxyFactoryCallBackFilter implements CallbackFilter {
+        @Override
+        public int accept(Method method) {
+            if ()
+        }
     }
 
 
