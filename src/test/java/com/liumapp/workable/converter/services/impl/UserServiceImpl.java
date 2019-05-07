a06d47e715dd751bf405a95da85335a64814b651
liumapp <liumapp.com@gmail.com>
2019-05-07 10:41:13 +0800

    add user service impl class

---
+++ src/test/java/com/liumapp/workable/converter/services/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package com.liumapp.workable.converter.services.impl;
+
+import com.liumapp.workable.converter.beans.User;
+import com.liumapp.workable.converter.services.UserService;
+
+/**
+ * file UserServiceImpl.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/7
+ */
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public boolean add(User name) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(User name) {
+        return false;
+    }
+
+    @Override
+    public User get(int id) {
+        return null;
+    }
+
+}
