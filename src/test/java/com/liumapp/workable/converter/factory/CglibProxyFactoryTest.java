26791d0f71656bec80fb4482f93eddfd12ee2805
liumapp <liumapp.com@gmail.com>
2019-05-07 11:00:37 +0800

    add slf4j dependencies

--- src/test/java/com/liumapp/workable/converter/factory/CglibProxyFactoryTest.java
+++ src/test/java/com/liumapp/workable/converter/factory/CglibProxyFactoryTest.java
@@ -1,6 +1,9 @@
 package com.liumapp.workable.converter.factory;
 
+import com.liumapp.workable.converter.invoker.ObjectInvoker;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
 
 import static org.junit.Assert.*;
 
@@ -9,6 +12,11 @@ public class CglibProxyFactoryTest {
     @Test
     public void createInvokerProxy() {
         ProxyFactory proxyFactory = new CglibProxyFactory();
-
+        ObjectInvoker logInvoker = new ObjectInvoker() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object... arguments) throws Throwable {
+                return null;
+            }
+        };
     }
 }
