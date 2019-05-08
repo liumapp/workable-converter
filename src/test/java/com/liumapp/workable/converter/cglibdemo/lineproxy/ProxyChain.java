5d063155b05f1072078d507085148d628a6a96f3
liumapp <liumapp.com@gmail.com>
2019-05-08 15:43:17 +0800

    add line proxy package

---
+++ src/test/java/com/liumapp/workable/converter/cglibdemo/lineproxy/ProxyChain.java
@@ -0,0 +1,15 @@
+package com.liumapp.workable.converter.cglibdemo.lineproxy;
+
+/**
+ * file ProxyChain.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/8
+ */
+public class ProxyChain {
+
+
+
+}
