97f5819a7e0454356e3717cb02abdb928716d422
liumapp <liumapp.com@gmail.com>
2019-05-08 15:49:57 +0800

    update proxy chain class info

--- src/test/java/com/liumapp/workable/converter/cglibdemo/lineproxy/ProxyChain.java
+++ src/test/java/com/liumapp/workable/converter/cglibdemo/lineproxy/ProxyChain.java
@@ -1,5 +1,12 @@
 package com.liumapp.workable.converter.cglibdemo.lineproxy;
 
+import lombok.Data;
+import net.sf.cglib.proxy.MethodProxy;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
 /**
  * file ProxyChain.java
  * author liumapp
@@ -8,8 +15,43 @@ package com.liumapp.workable.converter.cglibdemo.lineproxy;
  * homepage http://www.liumapp.com
  * date 2019/5/8
  */
+@Data
 public class ProxyChain {
 
+    private List<Proxy> proxyList;
 
+    private int currentProxyIndex = 0;
 
+    private Class<?> targetClass;
+
+    private Object targetObject;
+
+    private Method targetMethod;
+
+    private Object[] methodParams;
+
+    private MethodProxy methodProxy;
+
+    private Object methodResult;
+
+    public ProxyChain(Class<?> targetClass, Object targetObject, Method targetMethod, Object[] methodParams, MethodProxy methodProxy, Object methodResult) {
+        this.targetClass = targetClass;
+        this.targetObject = targetObject;
+        this.targetMethod = targetMethod;
+        this.methodParams = methodParams;
+        this.methodProxy = methodProxy;
+        this.methodResult = methodResult;
+    }
+
+    public void doProxyChain () {
+        if (currentProxyIndex < proxyList.size()) {
+            proxyList.get(currentProxyIndex++).doProxy(this);
+        } else {
+            try {
+                methodResult = methodProxy.invokeSuper(targetObject, methodParams);
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }
+    }
 }
