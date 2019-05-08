905f8f3430dc1b8e2ff7cf641752627e9cccc21f
liumapp <liumapp.com@gmail.com>
2019-05-08 10:50:53 +0800

    add add girl user only class

---
+++ src/test/java/com/liumapp/workable/converter/decoratordemo/AddGirlUserOnly.java
@@ -0,0 +1,15 @@
+package com.liumapp.workable.converter.decoratordemo;
+
+/**
+ * file AddGirlUserOnly.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/8
+ */
+public class AddGirlUserOnly {
+
+
+
+}
--- src/test/java/com/liumapp/workable/converter/decoratordemo/AddVipUserOnly.java
+++ src/test/java/com/liumapp/workable/converter/decoratordemo/AddVipUserOnly.java
@@ -6,16 +6,16 @@ import com.liumapp.workable.converter.services.UserService;
 
 /**
  * 执行get之前，需要校验口令
- * file VipUserOnly.java
+ * file AddVipUserOnly.java
  * author liumapp
  * github https://github.com/liumapp
  * email liumapp.com@gmail.com
  * homepage http://www.liumapp.com
  * date 2019/5/8
  */
-public class VipUserOnly extends UserDecorator {
+public class AddVipUserOnly extends UserDecorator {
 
-    public VipUserOnly(UserService userService) {
+    public AddVipUserOnly(UserService userService) {
         super(userService);
     }
 
@@ -27,16 +27,6 @@ public class VipUserOnly extends UserDecorator {
             return false;
         }
 
-    }
-
-    @Override
-    public boolean delete(int id) {
-        return super.delete(id);
-    }
-
-    @Override
-    public User get(int id) {
-        return super.get(id);
     }
 
     public boolean check (RoleEnums role) {
