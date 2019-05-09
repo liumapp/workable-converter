40d38f4e73400f8a31c0e81ea3ceb9fd81e04267
liumapp <liumapp.com@gmail.com>
2019-05-09 14:13:29 +0800

    update converter config manager

--- src/main/java/com/liumapp/workable/converter/factory/ConverterConfigManager.java
+++ src/main/java/com/liumapp/workable/converter/factory/ConverterConfigManager.java
@@ -1,5 +1,7 @@
 package com.liumapp.workable.converter.factory;
 
+import com.liumapp.workable.converter.config.ConverterConfig;
+
 /**
  * file ConverterConfigManager.java
  * author liumapp
@@ -9,6 +11,21 @@ package com.liumapp.workable.converter.factory;
  * date 2019/5/9
  */
 public class ConverterConfigManager {
+
+    private static ConverterConfigManager INSTANCE;
+
+    private static ConverterConfig converterConfig;
+
+    private ConverterConfigManager() {
+    }
+
+    public static ConverterConfigManager getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new ConverterConfigManager();
+            converterConfig = new ConverterConfig();
+        }
+        return INSTANCE;
+    }
 
 
 
