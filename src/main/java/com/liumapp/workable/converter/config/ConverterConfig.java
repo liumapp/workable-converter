7c85107e27daeb513290fa2423b498f749b75e12
liumapp <liumapp.com@gmail.com>
2019-05-09 11:20:27 +0800

    add qtools-property dependencies

---
+++ src/main/java/com/liumapp/workable/converter/config/ConverterConfig.java
@@ -0,0 +1,15 @@
+package com.liumapp.workable.converter.config;
+
+/**
+ * file ConverterConfig.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/9
+ */
+public class ConverterConfig {
+
+
+
+}
