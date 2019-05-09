096e7f63520844971aa2b00914535d73752f25e8
liumapp <liumapp.com@gmail.com>
2019-05-09 11:35:51 +0800

    add workable converter manager class with single factory mode

---
+++ src/main/java/com/liumapp/workable/converter/factory/WorkableConverterManager.java
@@ -0,0 +1,24 @@
+package com.liumapp.workable.converter.factory;
+
+/**
+ * file WorkableConverterManager.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/9
+ */
+public class WorkableConverterManager {
+
+    private static WorkableConverterManager INSTANCE;
+
+    private WorkableConverterManager() {
+    }
+
+    public WorkableConverterManager getInstance () {
+        if (INSTANCE == null) {
+            INSTANCE = new WorkableConverterManager();
+        }
+        return INSTANCE;
+    }
+}
