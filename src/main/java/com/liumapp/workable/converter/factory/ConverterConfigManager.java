07b590ad7da82e5aecb2c66b6e221357af55224c
liumapp <liumapp.com@gmail.com>
2019-05-09 14:55:38 +0800

    add junit test case for converter config manager

--- src/main/java/com/liumapp/workable/converter/factory/ConverterConfigManager.java
+++ src/main/java/com/liumapp/workable/converter/factory/ConverterConfigManager.java
@@ -3,6 +3,8 @@ package com.liumapp.workable.converter.factory;
 import com.liumapp.workable.converter.config.BasicLoadingConfigService;
 import com.liumapp.workable.converter.config.ConverterConfig;
 import com.liumapp.workable.converter.core.LoadingConfig;
+import com.liumapp.workable.converter.decorators.BuildingDefaultParamsConfig;
+import com.liumapp.workable.converter.decorators.CheckingUrlSourceForParamsConfig;
 
 /**
  * file ConverterConfigManager.java
@@ -34,7 +36,8 @@ public class ConverterConfigManager {
     }
 
     private static void buildingConverterConfig (ConverterConfigManager INSTANCE) {
-        INSTANCE.params = new ConverterConfig();
-//        LoadingConfig service = new
+        LoadingConfig service = new CheckingUrlSourceForParamsConfig(new BuildingDefaultParamsConfig(new BasicLoadingConfigService()));
+        service.loadURL();
+        INSTANCE.params = service.loadConfig();
     }
 }
---
+++ src/test/java/com/liumapp/workable/converter/factory/ConverterConfigManagerTest.java
@@ -0,0 +1,13 @@
+package com.liumapp.workable.converter.factory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConverterConfigManagerTest {
+
+    @Test
+    public void getParams() {
+
+    }
+}
