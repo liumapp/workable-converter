0f62efcac8edb522fd5c9ece5a126c06dcd0c6e9
liumapp <liumapp.com@gmail.com>
2019-05-09 14:09:49 +0800

    update for building configs

---
+++ src/main/java/com/liumapp/workable/converter/decorators/BuildingDefaultParamsConfig.java
@@ -0,0 +1,23 @@
+package com.liumapp.workable.converter.decorators;
+
+import com.liumapp.workable.converter.core.LoadingConfig;
+import com.liumapp.workable.converter.templates.NormalConverterConfigLoaderTemplate;
+
+/**
+ * file BuildingDefaultParamsConfig.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/9
+ */
+public class BuildingDefaultParamsConfig extends NormalConverterConfigLoaderTemplate {
+
+    public BuildingDefaultParamsConfig(LoadingConfig loadingConfig) {
+        super(loadingConfig);
+    }
+
+    
+
+
+}
