86680531ff6d3612722942f344c9ddc2c3897c0c
liumapp <liumapp.com@gmail.com>
2019-05-10 19:58:00 +0800

    tmp

--- src/main/java/com/liumapp/workable/converter/templates/NormalConverterTemplates.java
+++ src/main/java/com/liumapp/workable/converter/templates/NormalConverterTemplates.java
@@ -4,6 +4,14 @@ import com.liumapp.workable.converter.core.Converter;
 import com.liumapp.workable.converter.core.Templates;
 import com.liumapp.workable.converter.exceptions.ConvertFailedException;
 
+/**
+ * file NormalConverterTemplates.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/10
+ */
 public abstract class NormalConverterTemplates implements Converter, Templates {
 
     private Converter converter;
