9c2f211604afd9a1ac20d72435a28c3a031852d6
liumapp <liumapp.com@gmail.com>
2019-05-10 15:40:52 +0800

    add doc to png converter and its manager class

---
+++ src/main/java/com/liumapp/workable/converter/factory/DocToPngConverterManager.java
@@ -0,0 +1,34 @@
+package com.liumapp.workable.converter.factory;
+
+import com.liumapp.workable.converter.core.Converter;
+import com.liumapp.workable.converter.strategies.DocToPngConverter;
+
+/**
+ * file DocToPngConverterManager.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/10
+ */
+public class DocToPngConverterManager extends AbstractConverterManager {
+
+    private static class DocToPngConverterManagerHodler {
+        private static final DocToPngConverterManager INSTANCE = new DocToPngConverterManager();
+    }
+
+    private DocToPngConverterManager() {
+    }
+
+    public static Converter getInstance () {
+        return DocToPngConverterManagerHodler.INSTANCE.getConverter();
+    }
+
+    @Override
+    public Converter getConverter() {
+        if (this.converter == null) {
+            this.converter = new DocToPngConverter();
+        }
+        return this.converter;
+    }
+}
