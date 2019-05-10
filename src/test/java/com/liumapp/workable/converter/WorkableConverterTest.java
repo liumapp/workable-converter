a664728654360e76894b5ed81e06b5c1b594941c
liumapp <liumapp.com@gmail.com>
2019-05-10 20:01:44 +0800

    add workable converter junit test case

---
+++ src/test/java/com/liumapp/workable/converter/WorkableConverterTest.java
@@ -0,0 +1,32 @@
+package com.liumapp.workable.converter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * file WorkableConverterTest.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/10
+ */
+public class WorkableConverterTest {
+
+    @Test
+    public void convertByFilePath() {
+    }
+
+    @Test
+    public void convertByStream() {
+    }
+
+    @Test
+    public void convertByBase64() {
+    }
+
+    @Test
+    public void convert() {
+    }
+}
