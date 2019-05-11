73af54d063eabdc684d9a2bb7f372a83612a72ae
liumapp <liumapp.com@gmail.com>
2019-05-11 22:33:13 +0800

    add converter office manager class

---
+++ src/main/java/com/liumapp/workable/converter/factory/ConverterOfficeManager.java
@@ -0,0 +1,18 @@
+package com.liumapp.workable.converter.factory;
+
+import com.liumapp.workable.converter.core.Manager;
+
+/**
+ * file ConverterOfficeManager.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/11
+ */
+public class ConverterOfficeManager implements Manager {
+
+
+
+
+}
