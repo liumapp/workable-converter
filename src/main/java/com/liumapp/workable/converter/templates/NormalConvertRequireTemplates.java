7de8353867a0d7cc2b82a9d4c3f8eb8705e2bd5f
liumapp <liumapp.com@gmail.com>
2019-05-13 15:52:12 +0800

    add nomal convert require templates class

---
+++ src/main/java/com/liumapp/workable/converter/templates/NormalConvertRequireTemplates.java
@@ -0,0 +1,18 @@
+package com.liumapp.workable.converter.templates;
+
+import com.liumapp.workable.converter.core.Parameter;
+import com.liumapp.workable.converter.core.Templates;
+
+/**
+ * file NormalConvertRequireTemplates.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/13
+ */
+public class NormalConvertRequireTemplates implements Templates, Parameter {
+
+
+
+}
