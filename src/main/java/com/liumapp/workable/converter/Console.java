2afa93e5cd1aa690bfdab59be3e36be0ccfc6026
liumapp <liumapp.com@gmail.com>
2019-05-13 14:33:17 +0800

    ready to test console

--- src/main/java/com/liumapp/workable/converter/Console.java
+++ src/main/java/com/liumapp/workable/converter/Console.java
@@ -1,5 +1,8 @@
 package com.liumapp.workable.converter;
 
+import com.liumapp.workable.converter.config.ConvertRequire;
+import com.liumapp.workable.converter.exceptions.ConvertFailedException;
+import com.liumapp.workable.converter.factory.DocToPdfConverterManager;
 import com.liumapp.workable.converter.orders.Orders;
 import com.liumapp.workable.converter.proxies.ConverterProxy;
 import org.beryx.textio.TextIO;
@@ -19,29 +22,35 @@ public class Console {
 
     public static WorkableConverter workableConverter;
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws ConvertFailedException {
         textIO = TextIoFactory.getTextIO();
         textIO.getTextTerminal().getProperties().setPaneWidth(1280);
         textIO.getTextTerminal().getProperties().setPaneHeight(960);
 
         workableConverter = ConverterProxy.getInstance().getProxy();
-        
-
         showPrimaryMenu();
     }
 
-    private static void showPrimaryMenu () {
+    private static void showPrimaryMenu () throws ConvertFailedException {
         Orders orders = textIO.newEnumInputReader(Orders.class)
                 .read("choice your order...");
         handOrder(orders);
     }
 
-    private static void handOrder (Orders orders) {
+    private static void handOrder (Orders orders) throws ConvertFailedException {
+        ConvertRequire require = new ConvertRequire();
         switch (orders) {
             case ConvertDocToPDF:
-
+                require.setConvertByFilePathRequire(
+                        textIO.newStringInputReader().read("请输入要转换的doc文件地址（./data/test.doc）："),
+                        textIO.newStringInputReader().read("请输入转换后的pdf文件保存地址(./data/result.pdf)")
+                );
+                workableConverter.setConverterType(DocToPdfConverterManager.getInstance());
+                workableConverter.convertByFilePath(require);
+                textIO.dispose("转换成功！");
                 break;
             case ConvertHtmlToPDF:
+
                 break;
             case ConvertDocToPng:
                 break;
