51bdee0351aeb8add721bd5f071dd3a254e105b0
liumapp <liumapp.com@gmail.com>
2019-05-13 20:16:06 +0800

    :wheelchair: empty and null path is not allowed

--- src/main/java/com/liumapp/workable/converter/decorators/CheckingConvertPatternParamsDecorator.java
+++ src/main/java/com/liumapp/workable/converter/decorators/CheckingConvertPatternParamsDecorator.java
@@ -1,6 +1,7 @@
 package com.liumapp.workable.converter.decorators;
 
 import com.liumapp.qtools.file.basic.FileTool;
+import com.liumapp.qtools.str.basic.StrTool;
 import com.liumapp.workable.converter.config.ConvertRequire;
 import com.liumapp.workable.converter.core.ConvertPattern;
 import com.liumapp.workable.converter.exceptions.ConvertFailedException;
@@ -25,7 +26,9 @@ public class CheckingConvertPatternParamsDecorator extends NormalConvertRequireT
      */
     @Override
     public void setConvertByFilePathRequire(String srcWaitingConvertFilePath, String destConvertedFilePath) throws ConvertFailedException {
-        if (FileTool.isDirectory(srcWaitingConvertFilePath)) throw new ConvertFailedException(srcWaitingConvertFilePath + "must be a file path , can not be a folder");
+        if (StrTool.isSpace(srcWaitingConvertFilePath) || StrTool.isSpace(destConvertedFilePath)) throw new ConvertFailedException("src waiting path and dest conveted path can not be null or empty");
+        if (FileTool.isDirectory(srcWaitingConvertFilePath)) throw new ConvertFailedException(srcWaitingConvertFilePath + "must be a file path , and can not be a folder");
+        if (FileTool.isDirectory(destConvertedFilePath)) throw new ConvertFailedException(destConvertedFilePath + "must be file path , and can not be a folder ");
         super.setConvertByFilePathRequire(srcWaitingConvertFilePath, destConvertedFilePath);
     }
 
