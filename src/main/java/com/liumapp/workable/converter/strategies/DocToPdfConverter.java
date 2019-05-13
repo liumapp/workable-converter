d147637c48d7444736812c80dd0dc359a12c2c25
liumapp <liumapp.com@gmail.com>
2019-05-13 15:15:44 +0800

    should use decorator for convert require

--- src/main/java/com/liumapp/workable/converter/strategies/DocToPdfConverter.java
+++ src/main/java/com/liumapp/workable/converter/strategies/DocToPdfConverter.java
@@ -5,6 +5,14 @@ import com.liumapp.workable.converter.config.ConvertRequire;
 import com.liumapp.workable.converter.core.Parameter;
 import com.liumapp.workable.converter.exceptions.ConvertFailedException;
 
+/**
+ * file DocToPdfConverter.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/13
+ */
 public class DocToPdfConverter extends ConverterStrategy {
 
     @Override
@@ -13,7 +21,7 @@ public class DocToPdfConverter extends ConverterStrategy {
     }
 
     private <T> T convertAccordingRequire(ConvertRequire require) throws ConvertFailedException {
-        
+
         return null;
     }
 }
