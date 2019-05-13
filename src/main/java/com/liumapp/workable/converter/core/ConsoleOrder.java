943144d901193241edd80672751c9a6702ed2685
liumapp <liumapp.com@gmail.com>
2019-05-13 14:15:32 +0800

    add console order interface

---
+++ src/main/java/com/liumapp/workable/converter/core/ConsoleOrder.java
@@ -0,0 +1,12 @@
+package com.liumapp.workable.converter.core;
+
+/**
+ * file ConsoleOrder.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/13
+ */
+public interface ConsoleOrder {
+}
