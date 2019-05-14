a2666afa6856c5c79424031a651e0f05bd049339
liumapp <liumapp.com@gmail.com>
2019-05-14 15:19:46 +0800

    :green_heart: add common converter class

--- src/main/java/com/liumapp/workable/converter/strategies/CommonConverter.java
+++ src/main/java/com/liumapp/workable/converter/strategies/CommonConverter.java
@@ -4,6 +4,14 @@ import com.liumapp.workable.converter.config.ConvertRequire;
 import com.liumapp.workable.converter.core.Parameter;
 import com.liumapp.workable.converter.enums.Patterns;
 import com.liumapp.workable.converter.exceptions.ConvertFailedException;
+import org.jodconverter.JodConverter;
+import org.jodconverter.office.OfficeException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 /**
  * file CommonConverter.java
@@ -15,20 +23,32 @@ import com.liumapp.workable.converter.exceptions.ConvertFailedException;
  */
 public class CommonConverter extends ConverterStrategy {
 
+    private static Logger logger = LoggerFactory.getLogger(CommonConverter.class);
+
     @Override
     public boolean convert(Parameter require) throws ConvertFailedException {
+        logger.info("common converter begin at " + ZonedDateTime.now());
         return accordingRequire((ConvertRequire) require);
     }
 
-    private boolean accordingRequire (ConvertRequire require) {
+    private boolean accordingRequire (ConvertRequire require) throws ConvertFailedException {
         if (require.getPatterns() == Patterns.By_File_Path) {
             return byFilePath(require);
         }
         return false;
     }
 
-    private boolean byFilePath (ConvertRequire require) {
-
+    private boolean byFilePath (ConvertRequire require) throws ConvertFailedException {
+        logger.info("input file path is : " + require.getWaitingFilePath());
+        logger.info("output file path is : " + require.getResultFilePath());
+        File inputFile = new File(require.getWaitingFilePath());
+        File outputFile = new File(require.getResultFilePath());
+        try {
+            JodConverter.convert(inputFile).as(require.getSrcFormat())
+                    .to(outputFile).as(require.getDestFormat()).execute();
+        } catch (OfficeException e) {
+            throw new ConvertFailedException(e.getMessage());
+        }
         return true;
     }
 
