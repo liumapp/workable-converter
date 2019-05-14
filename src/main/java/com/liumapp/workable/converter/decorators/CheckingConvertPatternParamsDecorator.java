b1cc93f55c39291de544fa9f6232a56a7abdbcbd
liumapp <liumapp.com@gmail.com>
2019-05-14 11:40:43 +0800

    :hammer: check the result file , if existed , then delete

--- src/main/java/com/liumapp/workable/converter/decorators/CheckingConvertPatternParamsDecorator.java
+++ src/main/java/com/liumapp/workable/converter/decorators/CheckingConvertPatternParamsDecorator.java
@@ -23,12 +23,14 @@ public class CheckingConvertPatternParamsDecorator extends NormalConvertRequireT
 
     /**
      * check the path is a file ?
+     * if the result file existed, then delete
      */
     @Override
     public void setConvertByFilePathRequire(String srcWaitingConvertFilePath, String destConvertedFilePath) throws ConvertFailedException {
         if (StrTool.isSpace(srcWaitingConvertFilePath) || StrTool.isSpace(destConvertedFilePath)) throw new ConvertFailedException("src waiting path and dest conveted path can not be null or empty");
         if (FileTool.isDirectory(srcWaitingConvertFilePath)) throw new ConvertFailedException(srcWaitingConvertFilePath + "must be a file path , and can not be a folder");
         if (FileTool.isDirectory(destConvertedFilePath)) throw new ConvertFailedException(destConvertedFilePath + "must be file path , and can not be a folder ");
+        if (FileTool.isFileExists(destConvertedFilePath)) FileTool.deleteFile(destConvertedFilePath);
         super.setConvertByFilePathRequire(srcWaitingConvertFilePath, destConvertedFilePath);
     }
 
