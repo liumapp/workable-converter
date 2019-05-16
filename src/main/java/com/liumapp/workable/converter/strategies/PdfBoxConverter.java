dea91bc33dc5890f8b208c66444bd51f9d6f0b42
liumapp <liumapp.com@gmail.com>
2019-05-16 14:27:39 +0800

    :construction_worker: add new strategy for pdfbox

---
+++ src/main/java/com/liumapp/workable/converter/strategies/PdfBoxConverter.java
@@ -0,0 +1,22 @@
+package com.liumapp.workable.converter.strategies;
+
+import com.liumapp.workable.converter.core.Parameter;
+import com.liumapp.workable.converter.exceptions.ConvertFailedException;
+
+/**
+ * file PdfBoxConverter.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/5/16
+ */
+public class PdfBoxConverter extends ConverterStrategy {
+
+    @Override
+    public boolean convert(Parameter require) throws ConvertFailedException {
+        
+        return false;
+    }
+
+}
